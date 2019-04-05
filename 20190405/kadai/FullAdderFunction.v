b255785611d77884db3af02479d1977d81b0a5e3
Kawamura Shintaro <kawasin73@gmail.com>
2019-04-05 12:12:24 +0900

    Add homework for 20190405

---
+++ 20190405/kadai/FullAdderFunction.v
@@ -0,0 +1,9 @@
+module FullAdderFunction( x, y, cin, cout, s );
+
+    input x, y, cin;
+    output cout, s;
+
+    assign cout = (x & y) | (x & cin) | (y & cin);
+    assign s = x ^ y ^ cin;
+
+endmodule
